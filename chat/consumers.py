2799acaf100670872c0250cd663cbece777a0a6a
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-12-15 11:52:22 +0900

    ✨채팅 테스트케이스 작성,  User object를 가져올 수 있도록 구현

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,41 +1,64 @@
 import json
-from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import AccessToken, TokenError
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
+
+
+User = get_user_model()
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def get_user(self, user_id):
+        return User.objects.get(pk=user_id)
+
     async def connect(self):
         print("Scope:", self.scope)
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        token = self.scope["cookies"]["access_token"]
-        print("쿠키_access_token:", token)
-        user_id = AccessToken(token)
-        print("user_id:", user_id["user_id"])
+        try:
+            token = self.scope["cookies"]["access_token"]
+            user_id = AccessToken(token)["user_id"]
+
+            user = await self.get_user(user_id)
+            self.scope["user"] = user
+            print(user)
+            # print("user", self.scope["user"]) #AnonymousUser
+
+        except TokenError:
+            await self.close()
+            return Response({"error": "토큰만료"}, status=401)
+
         # 그룹 입장
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        # 권한 체크 필요
         await self.accept()
 
     async def disconnect(self, close_code):
-        # 떠나기
+        """
+        사용자와 WebSocket 연결이 끊겼을 때 호출
+        """
+        # 채팅방을 삭제하거나 보낸사람 or 받는사람에 대한 추가로직 작성필요
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # print("Message:", message)
-
+        user = self.scope["user"]
         # 그룹의 이벤트를 받기
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {"type": "chat.message", "username": user.nickname, "message": message},
         )
 
     async def chat_message(self, event):
         message = event["message"]
-        print(event)  # {'type': 'chat.message', 'message': 'ㄴㅇ'}
-        await self.send(text_data=json.dumps({"message": message}))
+        user = self.scope["user"]
+        print(event)  # {'type': 'chat.message', "username": user.pk, 'message': 'ㄴㅇ'}
+        await self.send(
+            text_data=json.dumps({"username": user.nickname, "message": message})
+        )
--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from channels.testing import WebsocketCommunicator
+from chat.consumers import ChatConsumer
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
 
-# Create your tests here.
+User = get_user_model()
+room_name = "test"
+email = "test12@test12.com"
+password = "testpw123"
+loginurl = "/accounts/login/"
+testmessage = "hello world"
+
+
+class MyTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email=email,
+            password=password,
+            gender="Male",
+            nickname="test",
+            birth="2023-11-11",
+        )
+        client = Client()
+        login = client.login(email=email, password=password)
+        self.assertTrue(login)
+
+    @database_sync_to_async
+    def login_user(self, client):
+        response = client.post(loginurl, {"email": email, "password": password})
+        return client
+
+    async def test_my_consumer(self):
+        communicator = WebsocketCommunicator(ChatConsumer.as_asgi(), "")
+        communicator.scope["url_route"] = {
+            "args": (),
+            "kwargs": {"room_name": room_name},
+        }
+        client = await self.login_user(Client())
+        self.assertTrue(client.login)
+
+        cookies = client.cookies.output(header="", sep=";").strip().split("; ")
+        communicator.scope["cookies"] = {}
+        for cookie in cookies:
+            key, value = cookie.split("=")
+            communicator.scope["cookies"][key] = value
+
+        connected, subprotocol_ = await communicator.connect()
+        self.assertTrue(connected)
+
+        print("----------메세지 전송 테스트-----------")
+        await communicator.send_json_to({"message": testmessage})
+        response = await communicator.receive_json_from()
+        print("받은 메세지:", response)
+        assert response["message"] == testmessage
+
+        await communicator.disconnect()
