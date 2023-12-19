2a36f5dc5839c8f0786222410e94232c72363b84
JYS <simjy93@gmail.com>
2023-12-19 13:27:02 +0900

    :adhesive_bandage: models.py 업데이트

--- companion/models.py
+++ companion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 class Tag(models.Model):
     tag = models.CharField(max_length=50,
@@ -29,6 +30,9 @@ class Companion(models.Model):
     image = models.ImageField(null=True, blank=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='A', blank=True)
     tags = models.ManyToManyField(Tag)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    max_member = models.IntegerField(default=2, null=False)
+    current_member = models.IntegerField(default=1, null=False)
 
     def __str__(self):
         return self.title
@@ -42,6 +46,7 @@ class Comment(models.Model):
     comment_text = models.TextField()
     parent_comment = models.ForeignKey('self', related_name='replies', null=True, blank=True, on_delete=models.CASCADE)
     companion_post = models.ForeignKey(Companion, related_name='comments', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Comment by {self.companion_post}: {self.comment_text[:50]}"
