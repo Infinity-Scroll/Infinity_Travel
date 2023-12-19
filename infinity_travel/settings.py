61343ad975989c6aeb45a2582a1cb62b9b3568e8
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-12-19 15:28:57 +0900

    이메일 환경설정

--- infinity_travel/settings.py
+++ infinity_travel/settings.py
@@ -188,3 +188,11 @@ SPECTACULAR_SETTINGS = {
     # },
     "VERSION": "1.0.0",
 }
+
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
